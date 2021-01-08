a78e3b4e0a10cb811de7d66d80af0f3bd6085f8d
Adam Chylaszek <adam.chylaszek98@gmail.com>
2021-01-08 18:59:46 +0100

    Collision detection, bricks renedering

---
+++ src/Arkanoid/Bricks.java
@@ -0,0 +1,71 @@
+package Arkanoid;
+
+import java.awt.*;
+import java.util.Random;
+
+public class Bricks {
+    public int bricks[][];
+    public Color bricksColors[][];
+    public int brickWidth = 45;
+    public int brickHeight = 20;
+
+    public Bricks(int row, int col) {
+        bricks = new int[row][col];
+        bricksColors = new Color[row][col];
+
+        for (int i = 0; i < bricks.length; i++) {
+            for (int j = 0; j < bricks[0].length; j++) {
+                bricks[i][j] = 1;
+                bricksColors[i][j] = randomColor();
+            }
+        }
+    }
+
+    public void drawBricks(Graphics2D g2d) {
+        for (int i = 0; i < bricks.length; i++) {
+            for (int j = 0; j < bricks[0].length; j++) {
+                if (bricks[i][j] > 0) {
+                    g2d.setColor(bricksColors[i][j]);
+                    g2d.fillRect(j * brickWidth + 32 + 10 * j, i * brickHeight + 80 + 10 * i, brickWidth, brickHeight);
+                }
+            }
+        }
+    }
+
+    public void setNewValueToBrick(int value, int row, int col) {
+        bricks[row][col] = value;
+    }
+
+    public Color randomColor() {
+        Random random = new Random();
+        int number = random.nextInt((6 + 1));
+        Color color;
+        switch (number) {
+            case 0:
+                color = Color.red;
+                break;
+            case 1:
+                color = Color.green;
+                break;
+            case 2:
+                color = Color.orange;
+                break;
+            case 3:
+                color = Color.yellow;
+                break;
+            case 4:
+                color = Color.magenta;
+                break;
+            case 5:
+                color = Color.cyan;
+                break;
+            case 6:
+                color = new Color(255, 111, 0);
+                break;
+            default:
+                color = Color.black;
+                break;
+        }
+        return color;
+    }
+}
--- src/Arkanoid/Game.java
+++ src/Arkanoid/Game.java
@@ -12,7 +12,8 @@ import java.io.IOException;
 public class Game extends JPanel implements MouseMotionListener, ActionListener {
     private boolean play = false;
     private int score = 0;
-
+    private int totalBricks = 50;
+    private Bricks bricks;
     private Timer timer;
     private int delay = 8;
     private int sliderPosition = 250;
@@ -21,6 +22,7 @@ public class Game extends JPanel implements MouseMotionListener, ActionListener 
     Image title = Toolkit.getDefaultToolkit().getImage("images/title.png");
 
     public Game(){
+        bricks = new Bricks(5, 10);
         addMouseMotionListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
@@ -29,28 +31,31 @@ public class Game extends JPanel implements MouseMotionListener, ActionListener 
     }
 
     public void paint(Graphics g){
-        //background image
+        // background image
         g.drawImage(background,0,0,this);
 
-        //borders
+        // borders
         g.setColor(Color.yellow);
         g.fillRect(10,60, 3, 705);
         g.fillRect(10,60, 580, 3);
         g.fillRect(590,60, 3, 705);
         g.fillRect(10,765, 583, 3);
 
-        //slider
+        // bricks
+        bricks.drawBricks((Graphics2D) g);
+
+        // slider
         g.setColor(Color.RED);
         g.fillRect(sliderPosition, 740, 100, 12 );
 
-        //ball
+        // ball
         g.setColor(Color.white);
         g.fillOval(ballPositionX, ballPositionY, 20, 20);
 
         // title draw
         g.drawImage(title, 5, 5, this);
 
-        //score print
+        // score print
         g.setColor(Color.yellow);
         Font scoreFont = null;
         try {
@@ -63,7 +68,7 @@ public class Game extends JPanel implements MouseMotionListener, ActionListener 
             e.printStackTrace();
         }
         g.setFont(scoreFont);
-        g.drawString("Score: " + score, 420, 50);
+        g.drawString("Score: " + score, 390, 50);
         g.dispose();
     }
 
@@ -91,6 +96,39 @@ public class Game extends JPanel implements MouseMotionListener, ActionListener 
             if(new Rectangle(ballPositionX, ballPositionY, 20, 20). intersects(new Rectangle(sliderPosition,740, 100, 15))){
                 ballDirectionY *= (-1);
             }
+
+            Collision: for (int i = 0; i < bricks.bricks.length; i++) {
+                for (int j = 0; j < bricks.bricks[0].length; j++) {
+                    if (bricks.bricks[i][j] > 0) {
+                        int brickX = j * bricks.brickWidth + 32 + 10 * j;
+                        int brickY = i * bricks.brickHeight + 80 + 10 * i;
+
+                        Rectangle brick = new Rectangle(brickX, brickY, bricks.brickWidth, bricks.brickHeight);
+                        Rectangle ball = new Rectangle(ballPositionX, ballPositionY, 20, 20);
+
+                        if (ball.intersects(brick)) {
+                            bricks.setNewValueToBrick(0, i, j);
+                            totalBricks--;
+
+                            if (Color.red.equals(bricks.bricksColors[i][j])) score += 2;
+                            else if (Color.green.equals(bricks.bricksColors[i][j])) score += 3;
+                            else if (Color.orange.equals(bricks.bricksColors[i][j])) score += 5;
+                            else if (Color.yellow.equals(bricks.bricksColors[i][j])) score += 10;
+                            else if (Color.magenta.equals(bricks.bricksColors[i][j])) score += 15;
+                            else if (Color.cyan.equals(bricks.bricksColors[i][j])) score += 25;
+
+                            if (ballPositionX + 19 <= brick.x || ballPositionX + 1 >= brick.x + brick.width) {
+                                ballDirectionX = -ballDirectionX;
+                            } else {
+                                ballDirectionY = -ballDirectionY;
+                            }
+
+                            break Collision;
+                        }
+                    }
+                }
+            }
+
             ballPositionX += ballDirectionX;
             ballPositionY += ballDirectionY;
             if(ballPositionX < 13){
